7cc0f413b7b8cb9c15224725ee98fb12ba3a71aa
Hubert <golab.mlody@gmail.com>
2022-05-06 20:17:03 +0200

    Create navBar with example routes

---
+++ src/components/NavMenu.vue
@@ -0,0 +1,101 @@
+<template>
+  <div id="navigationMenu">
+    <ul>
+      <li class="nav-menu-item">
+        <router-link :to="{ name: 'home'}">
+          <div class="px-2 py-3 item-content">
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
+              <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z"/>
+            </svg>
+            Strona główna
+          </div>
+        </router-link>
+      </li>
+      <li class="nav-menu-item">
+        <router-link :to="{ name: 'about'}">
+          <div class="px-2 py-3 item-content">
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-lines-fill" viewBox="0 0 16 16">
+              <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z"/>
+            </svg>
+            Zarządzanie kontami
+          </div>
+        </router-link>
+      </li>
+      <li class="nav-menu-item">
+        <router-link :to="{ name: 'home'}">
+          <div class="px-2 py-3 item-content">
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-motherboard-fill" viewBox="0 0 16 16">
+              <path d="M5 7h3V4H5v3Z"/>
+              <path d="M1 2a2 2 0 0 1 2-2h11a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-2H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 9H1V8H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 6H1V5H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 2H1Zm11 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7Zm2 0a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7ZM3.5 10a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1h-6Zm0 2a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1h-6ZM4 4h-.5a.5.5 0 0 0 0 1H4v1h-.5a.5.5 0 0 0 0 1H4a1 1 0 0 0 1 1v.5a.5.5 0 0 0 1 0V8h1v.5a.5.5 0 0 0 1 0V8a1 1 0 0 0 1-1h.5a.5.5 0 0 0 0-1H9V5h.5a.5.5 0 0 0 0-1H9a1 1 0 0 0-1-1v-.5a.5.5 0 0 0-1 0V3H6v-.5a.5.5 0 0 0-1 0V3a1 1 0 0 0-1 1Zm7 7.5v1a.5.5 0 0 0 .5.5h2a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-2a.5.5 0 0 0-.5.5Z"/>
+            </svg>
+            Stałe programowe
+          </div>
+        </router-link>
+      </li>
+      <li class="nav-menu-item">
+        <router-link :to="{ name: 'home'}">
+          <div class="px-2 py-3 item-content">
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cloud-download-fill" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M8 0a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 4.095 0 5.555 0 7.318 0 9.366 1.708 11 3.781 11H7.5V5.5a.5.5 0 0 1 1 0V11h4.188C14.502 11 16 9.57 16 7.773c0-1.636-1.242-2.969-2.834-3.194C12.923 1.999 10.69 0 8 0zm-.354 15.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 14.293V11h-1v3.293l-2.146-2.147a.5.5 0 0 0-.708.708l3 3z"/>
+            </svg>
+            Kopie zapasowe
+          </div>
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavMenu',
+}
+</script>
+
+<style scoped>
+#navigationMenu {
+  background: #313a46;
+  width: 260px;
+  bottom: 0;
+  position: fixed;
+  top: 0;
+  padding: 0;
+}
+
+ul{
+  padding: 0;
+  margin: 0;
+}
+
+.nav-menu-item {
+  list-style-type: none;
+  text-align: left;
+  margin-left: 40px;
+}
+
+.nav-menu-item a {
+  color: #8391a2;
+  text-decoration: none;
+}
+
+.nav-menu-item a:hover{
+  color: #bccee4;
+}
+
+.nav-menu-item a:active {
+  color: #fff;
+  text-decoration: none;
+}
+
+.item-content {
+  font-size: .9375rem;
+}
+
+router-link {
+  padding: 15px;
+}
+
+svg{
+  padding-bottom: 5px;
+}
+</style>
