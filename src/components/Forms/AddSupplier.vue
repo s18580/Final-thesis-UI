5aecfc501848c7ff1914e83079a4ffa742d65a87
Hubert <golab.mlody@gmail.com>
2022-05-11 17:18:38 +0200

    Change forms with vuestic

--- src/components/Forms/AddSupplier.vue
+++ src/components/Forms/AddSupplier.vue
@@ -2,36 +2,29 @@
   <div id="mainCo">
 		<form @submit.prevent="this.submitForm()">
 			<h1>Dodaj nowego dostawcę</h1>
-            <div class="input-box">
-				<label>Nazwa:</label>
-				<input v-model="supplierName" class="form-control" type="text">
-				<div v-if="nameErrorMessage" class="error-box">
-					<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-exclamation-triangle-fill error-icon" viewBox="0 0 16 16">
-						<path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-					</svg>
-                    <label class="error-message"> {{ nameErrorMessage }}</label>
-                </div>
-            </div>
-            <div class="input-box">
-				<label>Email:</label>
-				<input v-model="supplierEmail" class="form-control" type="text">
-				<div v-if="emailErrorMessage" class="error-box">
-					<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-exclamation-triangle-fill error-icon" viewBox="0 0 16 16">
-						<path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-					</svg>
-                    <label class="error-message"> {{ emailErrorMessage }}</label>
-                </div>
-			</div>
-            <div class="input-box">
-				<label>Telefon:</label>
-				<input v-model="supplierPhone" class="form-control" type="text">
-				<div v-if="phoneErrorMessage" class="error-box">
-					<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-exclamation-triangle-fill error-icon" viewBox="0 0 16 16">
-						<path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-					</svg>
-                    <label class="error-message"> {{ phoneErrorMessage }}</label>
-                </div>
-			</div>
+            <va-input
+                class="some-space mb-4"
+                v-model="supplierName"
+                :rules="[(v) => v.length > 0 || `Pole nazwa nie może być puste.`, (v) => v.length < 255 || `Pole nazwa przekroczyło limit znaków.`]"
+                label="Nazwa"
+                placeholder="Nazwa dostawcy"
+            />
+            <va-input
+                class="some-space mb-4"
+                v-model="supplierEmail"
+                :rules="[(v) => v.length > 0 || `Pole nazwa nie może być puste`, (v) => v.length < 255 || `Pole email przekroczyło limit znaków.`]"
+                label="Email"
+                placeholder="Email dostawcy"
+            />
+            <va-input
+                class="some-space mb-4"
+                v-model="supplierPhone"
+                :rules="[(v) => v.length > 0 || `Pole telefon jest nieprawidłowe.`, (v) => v.length < 255 || `Pole telefon przekroczyło limit znaków.`]"
+                label="Telefon"
+                placeholder="Telefon dostawcy"
+            />
+            
+            <va-divider inset />
 			<div id="card-container">
 				<div class="objects-card-wrapper">
 					<h6>Osoby kontaktowe dostawcy:</h6>
@@ -52,8 +45,9 @@
                             </div>
 						</div>
 					</div>
-					<button type="button" class="btn btn-info btn-sm">Dodaj osobę</button>
+                    <va-button type="button" color="success" gradient>Dodaj osobę</va-button>
 				</div>
+                <va-divider vertical />
 				<div class="objects-card-wrapper">
 					<h6>Adresy dostawcy:</h6>
 					<div class="objects-card">
@@ -73,20 +67,17 @@
                             </div>
 						</div>
 					</div>
-					<button type="button" class="btn btn-info btn-sm">Dodaj adres</button>
+                    <va-button type="button" color="success" gradient>Dodaj adres</va-button>
 				</div>
 			</div>
-			<div class="input-box">
-				<label>Opis (opcjonalnie):</label>
-				<textarea v-model="supplierDescription" class="form-control"></textarea>
-                <div v-if="descriptionErrorMessage" class="error-box">
-					<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-exclamation-triangle-fill error-icon" viewBox="0 0 16 16">
-						<path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-					</svg>
-                    <label class="error-message"> {{ descriptionErrorMessage }}</label>
-                </div>
-			</div>
-			<button type="submit" class="btn btn-primary my-3">Dodaj</button>
+            <va-input
+                class="mb-4 some-top-space"
+                v-model="supplierDescription"
+                type="textarea"
+                :rules="[ (v) => v.length < 255 || `Pole opis przekroczyło limit znaków.`]"
+                label="Opis (opcjonalnie)"
+            />
+            <va-button type="submit" color="info" gradient class="my-3">Dodaj</va-button>
 		</form>
 	</div>
 </template>
@@ -116,45 +107,7 @@ export default {
             }
 		},
         validateForm() {
-            let formIsCorrect = true;
-            this.nameErrorMessage = "";
-            this.emailErrorMessage = "";
-            this.phoneErrorMessage = "";
-            this.descriptionErrorMessage = "";
-
-            if(this.supplierName.trim().length == 0){
-                this.nameErrorMessage = "Pole nazwa nie może być puste.";
-                formIsCorrect = false;
-            } else if(this.supplierName.trim().length > 255) {
-                this.nameErrorMessage = "Pole nazwa przekroczyło limit znaków.";
-                formIsCorrect = false;
-            }
-
-            if(this.supplierEmail.trim().length != 0){
-                if(this.supplierEmail.trim().length > 255) {
-                    this.emailErrorMessage = "Pole email przekroczyło limit znaków.";
-                    formIsCorrect = false;
-                }
-            }
-
-            if(this.supplierPhone.trim().length != 0){
-                if(this.supplierPhone.trim().length < 9) {
-                    this.phoneErrorMessage = "Pole telefon jest nieprawidłowe.";
-                    formIsCorrect = false;
-                } else if(this.supplierPhone.trim().length > 32) {
-                    this.phoneErrorMessage = "Pole telefon przekroczyło limit znaków.";
-                    formIsCorrect = false;
-                }
-            }
-
-            if(this.supplierDescription.trim().length != 0){
-                if(this.supplierDescription.trim().length > 255) {
-                    this.descriptionErrorMessage = "Pole opis przekroczyło limit znaków.";
-                    formIsCorrect = false;
-                }
-            }
-
-            return formIsCorrect;
+            //check if form has errors
         }
 	}
 }
@@ -167,6 +120,18 @@ export default {
 	padding: 40px;
 	background: white;
 	border-radius: 25px;
+}
+
+h1 {
+    margin-bottom: 50px;
+}
+
+.some-space {
+    padding-bottom: 20px;
+}
+
+.some-top-space {
+    padding-top: 20px;
 }
 
 form {
