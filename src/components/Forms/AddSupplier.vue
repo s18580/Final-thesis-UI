1cf25481d26ddb9e1d5695d6e2622e7af51e28b9
Hubert <golab.mlody@gmail.com>
2022-05-13 08:54:42 +0200

    Add deleting items on click and redirection

--- src/components/Forms/AddSupplier.vue
+++ src/components/Forms/AddSupplier.vue
@@ -39,7 +39,7 @@
                                     <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                                 </svg>
 
-                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                <svg @click="removeContact(person.IdRepresentative)" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                                     <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                                 </svg>
                             </div>
@@ -61,13 +61,13 @@
                                     <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                                 </svg>
 
-                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                <svg @click="removeAddress(address.IdAddress)" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                                     <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                                 </svg>
                             </div>
 						</div>
 					</div>
-                    <va-button type="button" color="success" gradient>Dodaj adres</va-button>
+                    <va-button @click="AddD()" type="button" color="success" gradient>Dodaj adres</va-button>
 				</div>
 			</div>
             <va-input
@@ -100,16 +100,16 @@ export default {
 		}
 	},
 	methods: {
-		submitForm() {
+		async submitForm() {
             if(this.validateForm()) {
-                CallAPI.get(`/Address/getAddresses`)
-                    .then(res => console.log(res))
-                    .catch(err => {
-                        CallSeq.post('', {"Events":[{"Timestamp": new Date().toISOString(), "MessageTemplate": err.message, "Properties": { error: err }}]})
-                    });
-
-                // Added info
-                // Redirect on user main page
+                await CallAPI.get(`/Address/getAddresses`)
+                .then(res => {
+                    console.log(res); // switch that on the toast message
+                    this.$router.push('home');
+                })
+                .catch(err => {
+                    CallSeq.post('', {"Events":[{"Timestamp": new Date().toISOString(), "MessageTemplate": err.message, "Properties": { error: err }}]})
+                });
             }
 		},
         validateForm() {
@@ -121,6 +121,12 @@ export default {
 
             return this.isFormValidate;
         },
+        removeAddress(id) {
+            this.suplierAddresses = this.suplierAddresses.filter(item => item.IdAddress !== id);
+        },
+        removeContact(id) {
+            this.contactPepole = this.contactPepole.filter(item => item.IdAddress !== id);
+        }
 	},
 }
 </script>
@@ -196,7 +202,7 @@ h1 {
 	border-style: solid;
     border-radius: 10px;
 	width: 250px;
-	height: 300px;
+	max-height: 300px;
 	margin-bottom: 10px;
 	display: flex;
 	flex-direction: column;
