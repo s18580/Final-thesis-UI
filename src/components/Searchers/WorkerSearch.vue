7f528cc198d855d6012d916c55bae9d03e9aa59e
Hubert <golab.mlody@gmail.com>
2022-05-16 21:28:23 +0200

    Fix show more button for all searchers

--- src/components/Searchers/WorkerSearch.vue
+++ src/components/Searchers/WorkerSearch.vue
@@ -35,14 +35,16 @@
             </div>
         </div>
       </div>
-      <div @click="changeMode()" id="show-more">
-            Pokaż więcej filtrów
-            <svg v-if="!largeMode" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
-            </svg>
-            <svg v-if="largeMode" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-dash-lg" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M2 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11A.5.5 0 0 1 2 8Z"/>
-            </svg>
+      <div id="show-more">
+            <div @click="changeMode()" id="inner-show-more">
+                Pokaż więcej filtrów
+                <svg v-if="!largeMode" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
+                </svg>
+                <svg v-if="largeMode" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-dash-lg" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M2 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11A.5.5 0 0 1 2 8Z"/>
+                </svg>
+            </div>
       </div>
       <va-button @click="searchForResults()" color="info" gradient>Szukaj</va-button>
 	</div>
@@ -142,9 +144,16 @@ select option {
 
 #show-more {
     color: #2C82E0;
-    cursor: pointer;
     position: relative;
     text-align: right;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+}
+
+#inner-show-more {
+    cursor: pointer;
+    max-width: 170px;
 }
 
 </style>
