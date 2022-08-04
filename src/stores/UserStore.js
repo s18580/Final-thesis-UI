3041d9beb4c5fe36c8cc3a977d9db2d8ebcd1ac0
Hubert <golab.mlody@gmail.com>
2022-08-04 21:28:06 +0200

    Replace VueX with Pinia

---
+++ src/stores/UserStore.js
@@ -0,0 +1,28 @@
+import { defineStore } from 'pinia'
+
+export const useUserStore = defineStore('UserStore', {
+    state: () => {
+        return {
+            name: "Hubert Gołębiowski",
+            userToken: "",
+            userRefreshToken: "",
+            roles: [],
+        }
+    },
+
+    getters: {
+        userName() {
+            return this.name;
+        },
+        userRoles() {
+            return this.roles;
+        },
+        isUserAuthenticated() {
+            return true;
+        }
+    },
+
+    actions: {
+
+    }
+  })
