37e02f6303eaa19dc610de99fe69bc95cf433a9d
s18580 <s18580@pjwstk.edu.pl>
2022-08-07 20:44:52 +0200

    Add BasicHome page

---
+++ src/components/HomePages/BasicHome.vue
@@ -0,0 +1,73 @@
+<template>
+    <div id="background">
+        <div id="yoursOrderHeader" class="sectionHeader">
+            <h1>Twoje zamówienia</h1>
+            <va-divider />
+        </div>
+        <div id="yoursOrderTable" class="mainCo">
+            <OrdersTable :columns="yourColumns"/>
+        </div>
+        <div id="closestOrderHeader" class="sectionHeader">
+            <h1>Zamówienia z najbliższym terminem wykonania</h1>
+            <va-divider />
+        </div>
+        <div id="closestOrderTable" class="mainCo">
+            <OrdersTable :columns="closeColumns"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import OrdersTable from '@/components/ReuseComponents/OrdersTable.vue'
+
+export default {
+    name: 'BasicHome',
+    data() {
+		return {
+            yourColumns: [
+                { key: 'Identifier', label:"Identyfikator", sortable: true },
+                { key: 'Name', label:"Nazwa", sortable: true },
+                { key: 'CreationDate', label:"Data stworzenia", sortable: true },
+                { key: 'OrderItemsNames', label:"Przedmioty zamówienia" },
+                { key: 'StatusName', label:"Status" },
+                { key: 'IsAuction', label:"Przetarg" },
+                { key: 'actions', label:"Akcje", width: 80 },
+            ],
+            closeColumns: [
+                { key: 'Identifier', label:"Identyfikator", sortable: true },
+                { key: 'Name', label:"Nazwa", sortable: true },
+                { key: 'CreationDate', label:"Data stworzenia", sortable: true },
+                { key: 'OrderItemsNames', label:"Przedmioty zamówienia" },
+                { key: 'StatusName', label:"Status" },
+                { key: 'IsAuction', label:"Przetarg" },
+                { key: 'actions', label:"Akcje", width: 80 },
+            ],
+		}
+	},
+    components: { OrdersTable }
+}
+</script>
+
+<style scoped>
+.mainCo {
+	padding: 0px;
+    margin-right: 100px;
+    margin-left: 100px;
+    padding-left: 20px;
+    padding-right: 20px;
+	background: white;
+	border-radius: 25px;
+}
+
+#yoursOrderHeader {
+    margin-right: 100px;
+    margin-left: 100px;
+    margin-top: 20px;
+}
+
+#closestOrderHeader {
+    margin-right: 100px;
+    margin-left: 100px;
+    margin-top: 120px;
+}
+</style>
