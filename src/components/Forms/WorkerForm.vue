18e88becb6bbb300cda504c2d64c013046cafbda
s18580 <s18580@pjwstk.edu.pl>
2022-08-09 15:18:08 +0200

    Fix WorkerForm input structure

--- src/components/Forms/WorkerForm.vue
+++ src/components/Forms/WorkerForm.vue
@@ -10,55 +10,59 @@
             <div id="mainCoInner">
             <va-form @submit.prevent="this.submitForm()" id="form" tag="form" ref="form" @validation="isFormValidate = $event">
                 <va-input
-                    class="some-space mb-4"
+                    class="gridFirstC gridFirstR inputWidth"
                     v-model="workerName"
                     :rules="[(v) => v.length > 0 || `Pole imię nie może być puste.`, (v) => v.length < 33 || `Pole imię przekroczyło limit znaków.`]"
                     label="Imię"
                     placeholder="Imię pracownika"
                 />
                 <va-input
-                    class="some-space mb-4"
+                    class="gridSecondC gridFirstR inputWidth"
                     v-model="workerLastName"
                     :rules="[(v) => v.length > 0 || `Pole nazwisko nie może być puste.`, (v) => v.length < 65 || `Pole nazwisko przekroczyło limit znaków.`]"
                     label="Nazwisko"
                     placeholder="Nazwisko pracownika"
                 />
                 <va-input
-                    class="some-space mb-4"
+                    class="gridFirstC gridSecondR inputWidth"
                     v-model="workerEmail"
                     :rules="[(v) => v.length > 0 || `Pole email nie może być puste`, (v) => v.length < 256 || `Pole email przekroczyło limit znaków.`]"
                     label="Email"
                     placeholder="Email pracownika"
                 />
                 <va-input
-                    class="some-space mb-4"
+                    class="gridSecondC gridSecondR inputWidth"
                     v-model="workerPhone"
                     :rules="[(v) => v.length < 33 || `Pole telefon przekroczyło limit znaków.`]"
                     label="Telefon"
                     placeholder="Telefon pracownika"
                 />
-                <va-select
-                    class="some-space mb-4"
-                    v-model="selectedWorksite"
-                    :options="worksites"
-                    label="Stanowisko"
-                    placeholder="Bez stanowiska"
-                />
+                <div class="gridSpreadC gridFourthR inputWidth">
+                    <va-select
+                        class=""
+                        v-model="selectedWorksite"
+                        :options="worksites"
+                        label="Stanowisko"
+                        placeholder="Bez stanowiska"
+                    />
+                </div>
                 <va-input
-                    class="some-space mb-4"
+                    class="gridFirstC gridThirdR inputWidth"
                     v-model="passwordOne"
                     :rules="[(v) => v.length > 8 || `Hasło jest nieprawidłowe.`, (v) => v.length < 25 || `Hasło przekroczyło limit znaków.`, (v) => v == passwordTwo || `Hasła nie są takie same.`]"
                     label="Hasło"
                     placeholder="Hasło pracownika"
                 />
                 <va-input
-                    class="some-space mb-4"
+                    class="gridSecondC gridThirdR inputWidth"
                     v-model="passwordTwo"
                     :rules="[(v) => v == passwordOne || `Hasła nie są takie same.`]"
                     label="Powtórz hasło"
                     placeholder="Powtórz hasło pracownika"
                 />
-                <va-button type="submit" color="info" gradient class="my-3">Dodaj</va-button>
+                <div id="submitButtonContainer">
+                    <va-button id="submitButton" type="submit" color="info" gradient class="my-3">Dodaj</va-button>
+                </div>
             </va-form>
         </div>
         </div>
@@ -74,7 +78,7 @@
                                 {{ role.name }}
                             </div>
                             <div class="card-icons">
-                                <svg @click="removeRole(role.idRole)" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                <svg @click="removeRole(role)" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                                     <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                                 </svg>
                             </div>
@@ -175,19 +179,70 @@ export default {
             if(this.workerRoles.find(element => element.name == e.name) == undefined) {
                 this.workerRoles.push(e);
             }
+        },
+        removeRole(roleToDelete) {
+            const index = this.workerRoles.indexOf(roleToDelete);
+            if (index > -1) {
+                this.workerRoles.splice(index, 1);
+            }
         }
 	},
 }
 </script>
 
 <style scoped>
-.some-space {
-    padding-bottom: 20px;
+.inputWidth {
+    min-width: 300px;
 }
 
 #form {
-	padding-right: 150px;
-	padding-left: 150px;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+    justify-items: center;
+    align-items: center;
+}
+
+.gridSpreadC {
+    grid-column-start: 1;
+    grid-column-end: end;
+}
+
+.gridFirstC {
+    grid-column-start: 1;
+    grid-column-end: 1;
+}
+
+.gridFirstR {
+    grid-row-start: 1;
+    grid-row-end: 1;
+}
+
+.gridSecondC {
+    grid-column-start: 2;
+    grid-column-end: 2;
+}
+
+.gridSecondR {
+    grid-row-start: 2;
+    grid-row-end: 2;
+}
+
+.gridThirdR {
+    grid-row-start: 3;
+    grid-row-end: 3;
+}
+
+.gridFourthR {
+    grid-row-start: 4;
+    grid-row-end: 4;
+}
+
+#submitButtonContainer {
+    grid-column-start: 1;
+    grid-column-end: end;
+    grid-row-start: 5;
+    grid-row-end: 5;
 }
 
 #background {
@@ -271,6 +326,7 @@ export default {
 
 .card-icons svg{
     margin-left: 5px;
+    cursor: pointer;
 }
 
 .objects-card::-webkit-scrollbar {
