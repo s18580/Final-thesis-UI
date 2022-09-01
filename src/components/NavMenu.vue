0e50d3821ce179613f5b7e3788c3e4feddacff1a
s18580 <s18580@pjwstk.edu.pl>
2022-09-01 21:45:26 +0200

    nav items updates

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -67,16 +67,6 @@
           </div>
         </router-link>
       </li>
-      <li class="nav-menu-item" v-if="checkIfAuthorized('Admin')">
-        <router-link :to="{ name: 'home'}">
-          <div class="px-2 py-3 item-content">
-            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cloud-download-fill" viewBox="0 0 16 16">
-              <path fill-rule="evenodd" d="M8 0a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 4.095 0 5.555 0 7.318 0 9.366 1.708 11 3.781 11H7.5V5.5a.5.5 0 0 1 1 0V11h4.188C14.502 11 16 9.57 16 7.773c0-1.636-1.242-2.969-2.834-3.194C12.923 1.999 10.69 0 8 0zm-.354 15.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 14.293V11h-1v3.293l-2.146-2.147a.5.5 0 0 0-.708.708l3 3z"/>
-            </svg>
-            Kopie zapasowe
-          </div>
-        </router-link>
-      </li>
       <li class="nav-menu-item" v-if="checkIfAuthorized('Basic')">
           <div class="px-2 py-3 item-content nonclicable item-content2"  data-bs-toggle="collapse" data-bs-target="#collapseOrders" role="button" aria-expanded="false" aria-controls="collapseExample">
             <div>
@@ -105,11 +95,6 @@
           <router-link :to="{ name: 'OngoingOrders'}">
           <div class="px-2 py-3 item-content inner">
             W trakcie realizacji
-          </div>
-          </router-link>
-          <router-link :to="{ name: 'home'}">
-          <div class="px-2 py-3 item-content inner">
-            Kolejka produkcyjna
           </div>
           </router-link>
         </div>
