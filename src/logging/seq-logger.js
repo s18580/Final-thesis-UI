a2d83587ce217663594cae728db2dd55f44c8938
s18580 <s18580@pjwstk.edu.pl>
2023-01-10 00:02:21 +0100

    Change serilog ip to prod

--- src/logging/seq-logger.js
+++ src/logging/seq-logger.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const CallSeq = axios.create({
-  baseURL: `http://localhost:8081/api/events/raw`,
+  baseURL: `http://20.55.97.173:8081/api/events/raw`,
   headers: {
     Accept: 'application/json',
   }
